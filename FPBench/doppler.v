34f036f5fb80f15205481606010596e511e6f765
Ariel <ak2485@cornell.edu>
2022-10-07 11:41:30 -0400

    update doppler

--- FPBench/doppler.v
+++ FPBench/doppler.v
@@ -31,7 +31,6 @@ eexists. intro. prove_roundoff_bound.
 time "prove_rndval" prove_rndval; time "interval" interval.
 -
 time "prove_roundoff_bound2" prove_roundoff_bound2.
-
 match goal with |- Rabs (?u/?v * (1+?d') +?e' - - ?u1' * ?u2' * / ?v2') <= _ => 
 let u1:= fresh "u" in set (u1:= u);
 let v1:= fresh "v" in set (v1:= v);
@@ -41,96 +40,128 @@ let u3:= fresh "u" in set (u3:=- u1' * u2');
 let v3:= fresh "v" in set (v3:= v2');
 replace (u3 * / v3) with (u3/v3); try nra;
 replace (u2/v1 + e' - u3/v3) with (u2/v1 - u3/v3 + e'); try nra
-end.
-eapply Rle_trans.
-apply Rabs_triang.
-apply Rplus_le_compat.
-eapply Rle_trans.
-apply Rdiv_rel_error_add; 
+end ;
+eapply Rle_trans; [apply Rabs_triang | idtac ];
+apply Rplus_le_compat; 
+[eapply Rle_trans;  [apply Rdiv_rel_error_add;
 repeat match goal with |- ?g <> 0 =>
 try subst g; try interval
-end.
-eapply Rle_trans.
+end
+| idtac] | apply E8];
+eapply Rle_trans;
+[
+apply Rmult_le_compat; 
+[
+try apply Rabs_pos;
+try apply Rmult_le_pos;
+try apply Rplus_le_le_0_compat;
+try apply Rmult_le_pos; try apply Rabs_pos;
+try apply Rmult_le_pos; try apply Rabs_pos;
+try apply Rabs_pos 
+| 
+try apply Rabs_pos;
+try apply Rmult_le_pos;
+try apply Rplus_le_le_0_compat;
+try apply Rmult_le_pos; try apply Rabs_pos;
+try apply Rmult_le_pos; try apply Rabs_pos;
+try apply Rabs_pos
+| apply Rmult_le_compat;
+  [ try apply Rabs_pos;
+    try apply Rmult_le_pos;
+    try apply Rplus_le_le_0_compat;
+    try apply Rmult_le_pos; try apply Rabs_pos;
+    try apply Rmult_le_pos; try apply Rabs_pos;
+    try apply Rabs_pos
+  |apply Rabs_pos
+  |idtac
+  | subst v1 v2; idtac]
+| idtac]
+| idtac ].
+
+
+apply Rplus_le_compat;
 apply Rmult_le_compat;
 try apply Rabs_pos.
-apply Rmult_le_pos.
-apply Rplus_le_le_0_compat.
-apply Rmult_le_pos; try apply Rabs_pos.
-apply Rmult_le_pos; try apply Rabs_pos.
-try apply Rabs_pos.
-apply Rmult_le_compat;
-try apply Rabs_pos.
-try apply Rmult_le_pos.
-try apply Rplus_le_le_0_compat.
-apply Rmult_le_pos; try apply Rabs_pos.
-apply Rmult_le_pos; try apply Rabs_pos.
-apply Rplus_le_compat.
-apply Rmult_le_compat;
-try apply Rabs_pos.
-subst u0 u1 u.
-match goal with |- Rabs ?a <= _ =>
+
+subst u0 u1 u;
+time "field_simplify" match goal with |- Rabs ?a <= _ =>
 field_simplify a
 end.
 time "interval_intro" match goal with |- Rabs ?a <= _ =>
 interval_intro (Rabs a) with (i_bisect vxH, 
 i_bisect v, 
 i_bisect v0, i_depth 20) as H'
-end.
+end;
 apply H'.
-subst u1.
+
+subst u1;
+time "field_simplify" match goal with |- Rabs ?a <= _ =>
+field_simplify a; try (split ; interval)
+end.
 time "interval_intro" match goal with |- Rabs ?a <= _ =>
-interval_intro (Rabs a) with (i_bisect vxH, 
+interval_intro (Rabs a) with (
 i_bisect v, 
-i_bisect v0, i_depth 20) as H'
+i_bisect v0, i_depth 25) as H'
 end.
 apply H'.
-apply Rmult_le_compat;
-try apply Rabs_pos.
+
+
 subst v1 v2.
+(*time "interval_intro" match goal with |- Rabs ?a <= _ =>
+interval_intro (Rabs a) with (i_bisect vxH, 
+i_bisect v0, i_depth 20) as H'
+end.*)
+
+
 time "prune_terms" (prune_terms (cutoff 30)).
 match goal with |- (Rabs ?e <= ?a - ?b)%R =>
     let G := fresh "G" in
-    interval_intro (Rabs e) as G
+    interval_intro (Rabs e) with (i_bisect vxH, 
+i_bisect v0, i_depth 20) as G
 end.
-eapply Rle_trans.
-apply G.
-             apply Rminus_plus_le_minus;
-             apply Rle_refl.
+eapply Rle_trans;
+[
+apply G| apply Rminus_plus_le_minus; apply Rle_refl ].
+
+
+
 subst v2.
+time "field_simplify" match goal with |- Rabs ?a <= _ =>
+field_simplify a; try interval
+end.
 time "interval_intro" match goal with |- Rabs ?a <= _ =>
 interval_intro (Rabs a) with (i_bisect vxH, 
 i_bisect v, 
+i_bisect v0, i_depth 25) as H'
+end.
+apply H'.
+
+
+time "interval_intro" match goal with |- Rabs ?a <= _ =>
+interval_intro (Rabs a) with (i_bisect vxH,  
 i_bisect v0, i_depth 20) as H'
 end.
 apply H'.
-subst v1 v2.
+
+subst v2 u1.
+time "field_simplify" match goal with |- Rabs ?a <= _ =>
+field_simplify a; try interval
+end.
 time "interval_intro" match goal with |- Rabs ?a <= _ =>
 interval_intro (Rabs a) with (i_bisect vxH, 
 i_bisect v, 
-i_bisect v0, i_depth 20) as H'
+i_bisect v0, i_depth 25) as H'
 end.
 apply H'.
-             apply Rle_refl.
-apply Rmult_le_compat.
-try interval.
-apply Rabs_pos.
+
 apply Rle_refl.
-subst u1 v2.
-time "interval_intro" match goal with |- Rabs ?a <= _ =>
-interval_intro (Rabs a) with (i_bisect vxH, 
-i_bisect v, 
-i_bisect v0, i_depth 20) as H'
-end.
-apply H'.
-apply E8.
 Defined.
 
 Definition doppler1_bound_val := Eval simpl in doppler1_bound.
 Compute ltac:(ShowBound doppler1_bound_val).
 
-(*
 Lemma check :
-proj1_sig doppler1_bound_val <= 1.21e-12.
+proj1_sig doppler1_bound_val <= 1.21e-11.
 set (y:= proj1_sig doppler1_bound_val).
 simpl in y.
 subst y.
